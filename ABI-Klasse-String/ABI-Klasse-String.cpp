8af5d14134afd6990bc275bb031b97c0ca2a6373
44616E69656C <daniel.sz2002@gmail.com>
2023-03-17 16:47:57 +0100

    Klasse String Fertigstellen von: >Operatoren >Gegebenen Funktionen (siehe README.md)

--- ABI-Klasse-String/ABI-Klasse-String.cpp
+++ ABI-Klasse-String/ABI-Klasse-String.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
-#include <string>
 #include "String.h"
 using namespace std;
 
 int main()
 {
     string test = "string aus STL";
-    String str("Klasse String");
+    String str(";;Klasse;;String;");
+    String str2(";;Klasse;;String;");
+    String str3("Klasse String");
+    String empty;
+     
+    //std::cout << str << "  " << /*(str < test)*/ << endl; 
+    cout << "Split-function (on: ;; | spaces: -): " << str.split(";;")[0] << "-" << str.split(";;")[1] << "-" << str.split(";;")[2] << "-" << endl;
+    cout << "Empty (true): " << empty.isEmpty() << "  Empty (false): " << str.isEmpty() << endl;
+    cout << "StartsWith (true): " << str.startsWith(";;K") << "  StartsWith (false): " << str.startsWith(";K") << endl;
+    cout << "Equals (true): " << str.equals(str2) << "  Equals (true): " << str.equals(";;Klasse;;String;") << "  Equals (false): " << str.equals(str3) << endl;
+    cout << "Laenge-str (17): " << str.length() << endl;
+
 }
