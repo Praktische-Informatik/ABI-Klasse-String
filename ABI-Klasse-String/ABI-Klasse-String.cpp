55b51e2ca7253fdbeeb1870cc17b265e6cd80ef8
aschwaiger74 <78038701+aschwaiger74@users.noreply.github.com>
2023-04-27 10:58:00 +0200

    Code Teilkommentiert.

--- ABI-Klasse-String/ABI-Klasse-String.cpp
+++ ABI-Klasse-String/ABI-Klasse-String.cpp
@@ -1,12 +1,25 @@
-#include "String.h"
-#include <string>
+/* 
+ABI - Klasse_String.cpp ist nur eine Test - Datei.
+
+Beachte: Es wird zwischen "string" und "String" bei der Deklaration unterschieden!
+string --> C++ Methoden funktionieren
+String --> Java(-Simulation) Methoden funktionieren
+
+Der laufende Wechsel im Code zwischen String  und string muss mit Hilfe 
+von toC-String() (von Java-String zu Cpp-string) und 
+
+*/
+
+
+#include "String.h"  // String Klasse aus der Projektmappe. Muss includiert werden!
+#include <string>    // String Klasse aus der C++ STL Bibliothek. Muss includiert werden!
 using namespace std;
 
 int main()
 {
-    string test = "string aus STL";
-    String str(";;Klasse;;String;");
-    String zahl("1234");
+    string test = "string aus STL";   // Deklaration eines Cpp-Strings mit einer Initialisierung.
+    String str = ";;Klasse;;String;"; // Deklaration eines Java-Strings mit einer Initialisierung.
+    String zahl("1234");              // Alternative Deklaration eines Java-Strings mit einer Initialisierung.  
     String str2(";;Klasse;;String;");
     String str3("Klasse String");
     String empty;
@@ -15,8 +28,8 @@ int main()
     cout << "Eingabe eines Strings: ";
     cin >> NotEmpty;
     cout << NotEmpty << endl;
-    cout << stoi(zahl.toC_string()) << endl;
-    zahl = "1234+" + zahl + test + zahl;
+    cout << stoi(zahl.toC_string()) + 10 << endl;    // Umwandlung von einem Java-String in einen Cpp-String mit toC-String()  und dann einer Umwandlung in ein int mit stoi().
+    zahl = "1234+" + zahl + test + zahl;  //               
     cout << zahl << endl;
     cout << (string("234") > zahl);
     cout << ("zahl" < string("234"));
